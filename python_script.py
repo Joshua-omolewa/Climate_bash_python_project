663c955266da5706ea1d5f37083aa9edb8a4fce3
Joshua-omolewa <omolewajoshua@gmail.com>
2022-09-20 17:12:18 -0600

    done

--- python_script.py
+++ python_script.py
@@ -3,19 +3,22 @@
 #This python script combine three pandas data frames
 
 import pandas as pd
+import glob
 import os
 
-#print (os.getcwd()) #print current directory for confirmation script is running in correct directory
+print (os.getcwd()) #print current directory for confirmation script is running in correct directory
 #print(os.system('ls *.csv')) # To see the csv file in directory
+print(os.system('cd INPUT' && 'pwd'))
+
 
 #importing all the files
-file1 = pd.read_csv('/home/dataengr/weclouddata_project1/en_climate_hourly_NT_2204100_02-2020_P1H.csv') 
-file2 = pd.read_csv('/home/dataengr/weclouddata_project1/en_climate_hourly_NT_2204100_02-2021_P1H.csv')
-file3 = pd.read_csv('/home/dataengr/weclouddata_project1/en_climate_hourly_NT_2204100_02-2022_P1H.csv')
+# file1 = pd.read_csv('/home/dataengr/weclouddata_project1/en_climate_hourly_NT_2204100_02-2020_P1H.csv') 
+# file2 = pd.read_csv('/home/dataengr/weclouddata_project1/en_climate_hourly_NT_2204100_02-2021_P1H.csv')
+# file3 = pd.read_csv('/home/dataengr/weclouddata_project1/en_climate_hourly_NT_2204100_02-2022_P1H.csv')
 
 #combine all the files into one single data framemalong the rows
-combine_all_csv_files = pd.concat([file1,file2,file3],axis=0)
+# combine_all_csv_files = pd.concat([file1,file2,file3],axis=0)
 #print(combine_all_csv_files) 
 
 #export file name and exlude pandas index
-done=combine_all_csv_files.to_csv('all_years.csv', index=False)
+# done=combine_all_csv_files.to_csv('all_years.csv', index=False)
--- shell_script.sh
+++ shell_script.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
-#creating shell script to download the raw data and save file into 
+##### creating shell script to download the raw data and save file into ######
+
+### SETTING DATE FOR LOG FILES ###
+log_date=$(date +"%d %m %Y %H-%M-%S")
+
+### Setting enviroment vairable for all paths to folders ###
+
+export INPUT_FOLDER=/home/dataengr/weclouddata_project1/Weclouddata-PROJECT1/input
+export OUTPUT_FOLDER=/home/dataengr/weclouddata_project1/Weclouddata-PROJECT1/output
+export LOG_FOLDER= /home/dataengr/weclouddata_project1/Weclouddata-PROJECT1/logs
+
+
 
 
 : 'for loop below  download the data from canadian climate website using
@@ -8,7 +19,7 @@ wget command from 2020 -2022'
 
 for year in {2020..2022};do
 
-wget  --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=1706&Year=${year}&Month=2&Day=14&timeframe=1&submit= Download+Data";
+wget  --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=48549&Year=${year}&Month=2&Day=14&timeframe=2&submit= Download+Data " -0;
 done;
 
 
