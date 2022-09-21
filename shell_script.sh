23ca44b0ddc0ff4f8a537e9a9a2909245988a561
Joshua-omolewa <omolewajoshua@gmail.com>
2022-09-20 19:05:38 -0600

    shell script update

--- shell_script.sh
+++ shell_script.sh
@@ -4,12 +4,16 @@
 
 ### SETTING DATE FOR LOG FILES ###
 log_date=$(date +"%d %m %Y %H-%M-%S")
+##########################################
 
-### Setting enviroment vairable for all paths to folders ###
+### Setting enviroment variable for all paths to folders and log file ###
 
-export INPUT_FOLDER=/home/dataengr/weclouddata_project1/Weclouddata-PROJECT1/input
-export OUTPUT_FOLDER=/home/dataengr/weclouddata_project1/Weclouddata-PROJECT1/output
-export LOG_FOLDER= /home/dataengr/weclouddata_project1/Weclouddata-PROJECT1/logs
+export INPUT_FOLDER=/home/dataengr/project1/Climate_bash_python_project/input
+export OUTPUT_FOLDER=/home/dataengr/project1/Climate_bash_python_project/output
+export LOG_FOLDER=/home/dataengr/project1/Climate_bash_python_project/logs
+export LOG_FILE_NAME="shell_script"
+export LOG_FILE=${LOG_FOLDER}/${LOG_FILE_NAME}_$(log_date).log
+##############################################################
 
 
 
@@ -19,12 +23,12 @@ wget command from 2020 -2022'
 
 for year in {2020..2022};do
 
-wget  --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=48549&Year=${year}&Month=2&Day=14&timeframe=2&submit= Download+Data " -0;
+wget  --content-disposition "https://climate.weather.gc.ca/climate_data/bulk_data_e.html?format=csv&stationID=48549&Year=${year}&Month=2&Day=14&timeframe=2&submit= Download+Data " -O $(INPUT_FOLDER)/$(year).csv;
 done;
 
 
 #run python script
-./python_script.py
+#./python_script.py
 
 #checking if scripts run
 
